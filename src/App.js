9b55197512994065437eb3edd901887df0243dbd
Buttershine <gaedk003@gmail.com>
2018-07-16 16:11:36 -0500

    Upgraded api to v2. Refactored methods.

--- src/App.js
+++ src/App.js
@@ -37,6 +37,12 @@ class App extends React.Component {
         );
     }
 
+    getInitialState() {
+        return {
+            name: this.props.name
+        };
+    }
+
     constructor() {
         super();
         this.state = {
@@ -47,10 +53,72 @@ class App extends React.Component {
         this.handleInput = this.handleInput.bind(this);
     }
 
-    getInitialState() {
+    componentDidMount = async () => {
+        let _this = this;
+        let response = await this.makeRequest();
+        // response.result1.data.data.for(function(cmCoin) {
+        //     var token = response.result2.data.Data[cmCoin.symbol];
+        //     if(token) {
+        //         if (token.ImageUrl) {
+        //             cmCoin.image = 'https://www.cryptocompare.com' + token.ImageUrl;
+        //         }
+        //     }
+        // });
+
+        let list = response.result1.data.data.slice(0, 10); //assigns the array to the coinList object
+        let localStorageCoinList = this.getLocalStorageCoinList();
+
+        if(localStorageCoinList ? localStorageCoinList.length > 0 : false) {
+            list = await this.updateCoinList(localStorageCoinList, response.result1.data);
+            _this.setState({
+                coinList: list,
+                response: response
+            });
+        } else {
+            _this.setState({
+                coinList: list,
+                response: response
+            });
+        }
+
+        this.setPageTotals(_this,response, localStorageCoinList);
+
+    }
+
+    makeRequest = async () => {
+        const task1 = getRequest('https://api.coinmarketcap.com/v2/listings/');
+        const task2 = getRequest('https://min-api.cryptocompare.com/data/all/coinlist');
+        //const task3 = getRequest('https://coinlib.io/searchable_items_json?json');
+        //const task3 = getRequest('https://api.binance.com/api/v3/ticker/price');
+
         return {
-            name: this.props.name
-        };
+            result1: await task1,
+            result2: await task2,
+            //result3: await task3,
+            //result3: await task3
+        }
+    }
+
+    getLocalStorageCoinList = () => {
+        let localStorageCoinList = localStorage.getItem("coinList");
+
+        if(localStorageCoinList && localStorageCoinList !== "undefined") {
+            localStorageCoinList = JSON.parse(localStorage.getItem('coinList'));
+        } else {
+            localStorageCoinList = false;
+        }
+        return localStorageCoinList;
+    }
+
+    setPageTotals = (_this,response, localStorageCoinList) => {
+        let localStorageCoinListTotal = localStorage.getItem("coinListTotal");
+        if(localStorageCoinListTotal && localStorageCoinListTotal !== "undefined") {
+            let coinListTotal = this.calculateTotal(localStorageCoinList);
+            _this.setState({
+                coinListTotal: coinListTotal,
+                response: response
+            });
+        }
     }
 
     //Helper Methods
@@ -90,58 +158,27 @@ class App extends React.Component {
         });
     }
 
-    makeRequest = async () => {
-
-        const task1 = getRequest('https://api.coinmarketcap.com/v1/ticker/');
-        const task2 = getRequest('https://min-api.cryptocompare.com/data/all/coinlist');
-        //const task3 = getRequest('https://coinlib.io/searchable_items_json?json');
-        //const task3 = getRequest('https://api.binance.com/api/v3/ticker/price');
-
-        return {
-            result1: await task1,
-            result2: await task2,
-            //result3: await task3,
-            //result3: await task3
-        }
-    }
-
-    handleInput = (event) => {
+    handleInput = async (event) => {
         const symbol = event.target.parentElement.parentElement.id;
         const amount = event.target.value;
-        let fiatValue = '';
-        for(let token in this.props.data){
-            if(token.symbol === symbol){
-                fiatValue = this.props.data.price_usd * amount;
-            }
-        }
-        this.createElement(symbol, amount, fiatValue);
+        const response = await getRequest("https://api.coinmarketcap.com/v2/ticker/1/?convert="+symbol);
+        const token = {
+            symbol: symbol,
+            amount: amount,
+            fiatValue: this.calculatePriceFromBitcoin(response, symbol)
+        };
+        this.updateCoinList(this.state.coinList, response, token);
     }
 
     handleSearch = async (event) => {
         const symbol = event.target.value;
-        let response = await getRequest("https://api.coinmarketcap.com/v1/ticker/"+symbol+"/");
-        this.updateCoinList(this.state.coinList, response.data[0]);
-        //addCoin to list
-    }
-
-    createElement = (symbol, amount, fiatValue) => {
-        //dynamically created object is concatenated with state
-        let token = {
+        const response = await getRequest("https://api.coinmarketcap.com/v2/ticker/1/?convert="+symbol);
+        const token = {
             symbol: symbol,
-            amount: amount,
-            fiatValue: fiatValue
-        }
-
-        let newState = Object.assign({}, this.state);
-        newState.coinList = this.updateCoinList(this.state.coinList, this.state.response, token);
-        newState.coinListTotal = this.calculateTotal(newState.coinList);
-        this.setState({
-            coinList: newState.coinList,
-            coinListTotal: newState.coinListTotal
-        });
-        localStorage.setItem('coinList', JSON.stringify(newState.coinList));
-        localStorage.setItem('coinListTotal', JSON.stringify(newState.coinListTotal));
-
+            amount: 0,
+            fiatValue: this.calculatePriceFromBitcoin(response, symbol)
+        };
+        this.updateCoinList(this.state.coinList, response, token);
     }
 
     calculateTotal(coinList) {
@@ -155,25 +192,30 @@ class App extends React.Component {
         return total;
     }
 
-    updateCoinList = (currentCoinList, response, token) => {
+    calculatePriceFromBitcoin = (response, symbol) => {
+        if(symbol != 'BTC' && response.data.data.quotes[symbol]) {
+            return response.data.data.quotes.USD.price / response.data.data.quotes[symbol].price;
+        }
+        else return response.data.data.quotes.USD.price;
+    }
+
+    updateCoinList = async (currentCoinList, response, userInputAToken) => {
         let coinIsInList = false;
         for(let eachToken in currentCoinList) {
-            if(token) {
-                if(token.symbol === currentCoinList[eachToken].symbol) {
-                    currentCoinList[eachToken].amount = token.amount;
-                    currentCoinList[eachToken].price_usd = response.result1.data.find(x => x.symbol === token.symbol).price_usd; //The find method requires a generic function: x => x.name === token.name means search for name in the supplied array and return once found
-                }
-                coinIsInList = true;
-            }else {
-                if(currentCoinList[eachToken].symbol === response.symbol) {
+            let currentToken = currentCoinList[eachToken];
+            if(userInputAToken) {
+                if(userInputAToken.symbol === currentToken.symbol) {
+                    currentToken.amount = userInputAToken.amount;
                     coinIsInList = true;
                 }
             }
+            const updatedPriceOfTokenResponse = await getRequest("https://api.coinmarketcap.com/v2/ticker/1/?convert="+currentToken.symbol);
+            currentToken.price_usd = this.calculatePriceFromBitcoin(updatedPriceOfTokenResponse, currentToken.symbol);
         }
-        debugger;
-        if(!coinIsInList){
+        let newState = Object.assign({}, this.state);
+        if(userInputAToken && !coinIsInList) {
             currentCoinList.push(response);
-            let newState = Object.assign({}, this.state);
+        } else {
             newState.coinList = currentCoinList;
             newState.coinListTotal = this.calculateTotal(newState.coinList);
             this.setState({
@@ -181,62 +223,10 @@ class App extends React.Component {
                 coinListTotal: newState.coinListTotal
             });
         }
+        localStorage.setItem('coinList', JSON.stringify(newState.coinList));
+        localStorage.setItem('coinListTotal', JSON.stringify(newState.coinListTotal));
         return currentCoinList;
     }
-
-    updatePriceInCoinList = (currentCoinList, response) => {
-        for(let eachToken in currentCoinList) {
-                if(eachToken.symbol === response.symbol) {
-                    currentCoinList[eachToken].price_usd = response.result1.data.find(x => x.symbol === currentCoinList[eachToken].symbol).price_usd; //The find method requires a generic function: x => x.name === token.name means search for name in the supplied array and return once found
-                }
-
-        }
-        return currentCoinList;
-    }
-
-    componentDidMount = async () => {
-        let _this = this;
-        let response = await this.makeRequest();
-        response.result1.data.forEach(function(cmCoin) {
-            var token = response.result2.data.Data[cmCoin.symbol];
-            if(token) {
-                if (token.ImageUrl) {
-                    cmCoin.image = 'https://www.cryptocompare.com' + token.ImageUrl;
-                }
-            }
-        });
-
-        let list = response.result1.data.slice(0, 10); //assigns the array to the coinList object
-        let localStorageCoinList = localStorage.getItem("coinList");
-
-        if(localStorageCoinList && localStorageCoinList !== "undefined") {
-            localStorageCoinList = JSON.parse(localStorage.getItem('coinList'));
-        } else {
-            localStorageCoinList = false;
-        }
-
-        if(localStorageCoinList ? localStorageCoinList.length > 0 : false) {
-            list = this.updatePriceInCoinList(localStorageCoinList, response);
-            _this.setState({
-                coinList: list,
-                response: response
-            });
-        } else {
-            _this.setState({
-                coinList: list,
-                response: response
-            });
-        }
-
-        let localStorageCoinListTotal = localStorage.getItem("coinListTotal");
-            if(localStorageCoinListTotal && localStorageCoinListTotal !== "undefined") {
-                let coinListTotal = this.calculateTotal(localStorageCoinList);
-                _this.setState({
-                    coinListTotal: coinListTotal,
-                    response: response
-                });
-            }
-        }
 
 }
 
